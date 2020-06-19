463e42be2465daa147868c6db98384f4e0c7246b
Kirill Buziuk <last2828@gmail.com>
2020-06-19 10:34:37 +0300

    update

--- app/Http/Requests/FormValidator.php
+++ app/Http/Requests/FormValidator.php
@@ -48,7 +48,9 @@ class FormValidator extends FormRequest
 
             case 4: return [
                 'field-26' => 'required',
-                'field-27' => 'required',
+                'field-28' => 'required',
+                'field-29' => 'required',
+                'field-30' => 'required',
             ];
 
             case 5: return [
