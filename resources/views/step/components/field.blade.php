b1d28c76d0e02b2c7bd1ee13ef6493e9a02d901b
Kirill Buziuk <last2828@gmail.com>
2020-07-24 08:00:52 +0300

    update

--- resources/views/step/components/field.blade.php
+++ resources/views/step/components/field.blade.php
@@ -1,3 +1,4 @@
+@php($countries = include('../resources/views/step/components/countries.php'))
 @if(isset($data[1]))
   @foreach($data as $form_field)
     @if($form_field->field_id == $id)
@@ -23,23 +24,10 @@
 
       {{-- Set value from first step in five step --}}
       @if ($step->id == 5)
-        @if ($id == 40)
+        @if ($id == 40 or $id == 41 or $id == 42 or $id == 43 or $id == 44)
           value="{{(isset($data[0])) ? $data[0]->value : $data_step_1[0]->value}}"
-        @endif
-        @if ($id == 41)
-          value="{{(isset($data[0])) ? $data[1]->value : $data_step_1[1]->value}}"
-        @endif
-        @if ($id == 42)
-          value="{{(isset($data[0])) ? $data[2]->value : $data_step_1[2]->value}}"
-        @endif
-        @if ($id == 43)
-        value="{{(isset($data[0])) ? $data[3]->value : $data_step_1[3]->value}}"
-        @endif
-        @if ($id == 44)
-        value="{{(isset($data[0])) ? $data[4]->value : $data_step_1[4]->value}}"
-        @endif
-        @if ($id == 45)
-        value="{{(isset($data[0])) ? $data[5]->value : $data_step_1[5]->value}}"
+        @else
+          value="{{(isset($field_value) ? $field_value : '')}}"
         @endif
       @else
         value="{{(isset($field_value) ? $field_value : '')}}"
@@ -53,33 +41,31 @@
     </a>
     @endif
   @endif
-@elseif($type == 'select' and $id == 6)
-  @php($countries = include('../resources/views/step/components/countries.php'))
-  <div class="field" id="field-{{$id}}">
-    <label for="{{$id}}">{{$title}}</label>
-    <select name="field-{{$id}}" id="{{$id}}" @if ($required) required @endif class="@if ($errors->has(`field-{$id}`)) error @endif">
-      <option value=""></option>
-      @foreach ($countries as $key => $value)
-        @if(isset($field_value) and $field_value == $key)
-          <option selected value="{{$key}}">{{$value['name']}}</option>
-        @else
-          <option value="{{$key}}">{{$value['name']}}</option>
-        @endif
-      @endforeach
-    </select>
-  </div>
+
 @elseif($type == 'select')
   <div class="field" id="field-{{$id}}">
     <label for="{{$id}}">{{$title}}</label>
     <select name="field-{{$id}}" id="{{$id}}" @if ($required) required @endif class="@if ($errors->has(`field-{$id}`)) error @endif">
       <option value=""></option>
-      @foreach ($options as $option)
-        @if(isset($field_value) and $field_value == $option['id'])
-        <option id="option-{{$option['id']}}" selected value="{{$option['id']}}">{{$option['name']}}</option>
-        @else
-        <option id="option-{{$option['id']}}" value="{{$option['id']}}">{{$option['name']}}</option>
-        @endif
-      @endforeach
+
+      @if($id == 6 or $id == 45)
+        @foreach($countries as $key => $value)
+          @if(isset($field_value) and $field_value == $key or isset($data_step_1[0]) and $data_step_1[5]->value == $key)
+            <option selected value="{{$key}}">{{$value['name']}}</option>
+          @else
+            <option value="{{$key}}">{{$value['name']}}</option>
+          @endif
+        @endforeach
+      @else
+        @foreach ($options as $option)
+          @if(isset($field_value) and $field_value == $option['id'])
+            <option id="option-{{$option['id']}}" selected value="{{$option['id']}}">{{$option['name']}}</option>
+          @else
+            <option id="option-{{$option['id']}}" value="{{$option['id']}}">{{$option['name']}}</option>
+          @endif
+        @endforeach
+      @endif
+      
     </select>
   </div>   
 @endif
